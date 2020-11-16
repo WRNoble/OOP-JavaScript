d193a15b4cbb5f8a834ce3f5a13b7d204e6a1c3d
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-11-16 10:21:48 -0500

    factory function

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function () {
-    console.log("draw");
-  },
-};
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    },
+  };
+}
 
+const circle = createCircle(1);
 circle.draw();
